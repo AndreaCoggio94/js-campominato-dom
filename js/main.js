89051c0bf05544e0f6e1934879c1568d0101e5ca
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-08-22 12:04:35 +0200

    added function to generate random number

--- js/main.js
+++ js/main.js
@@ -4,12 +4,17 @@ const startButton = document.getElementById("button-start");
 const gameGrid = document.getElementById("game-grid");
 // get difficulty
 const difficulty = document.getElementById("game-difficulty");
+// possible bomb spots
+let bombSpots = [];
+// bomb array
+let bomb = [];
 
 // start button functionality
 
 startButton.addEventListener("click", function () {
   gameGrid.innerHTML = "";
   let grid = difficulty.value;
+  generateNumericProgressiveArray(1, grid * grid, 1, bombSpots);
   gridGenerator(grid);
 });
 
@@ -43,4 +48,12 @@ function generateNumericProgressiveArray(from, to, step, where) {
   for (let i = from; i <= to; i += step) {
     where.push(i);
   }
+  console.log(where);
+}
+
+// function to generate random number
+
+function generateRandomNumber(min, max) {
+  let number = Math.floor(Math.random() * max) + min;
+  console.log(number);
 }
